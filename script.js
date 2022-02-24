b84b363d4fc00ec6ef2f3e48df6e57e7d5571270
szeri <szeri323@gmail.com>
2022-02-24 15:41:55 +0100

    commit on main

--- script.js
+++ script.js
@@ -1,55 +1,10 @@
-/*document.addEventListener("keypress",function() {
-    console.log("pressed");d
-});*/
-/*const log = document.getElementById('log');
 
-document.addEventListener('keypress', logKey);
-
-function logKey(e) {
-    log.textContent += ` ${e.code}`;
-}*/
 window.addEventListener("keydown", function(event) {
-    let audio = new Audio(`/sounds/${event.key}.mp3`);
-    audio.play();
+    if(event.code === "KeyW" || event.code === "KeyE" || event.code === "KeyT" || event.code === "KeyY" || event.code === "KeyU" || event.code === "KeyA" || event.code === "KeyS" || event.code === "KeyD" || event.code === "KeyF" || event.code === "KeyG" || event.code === "KeyH" || event.code === "KeyJ") {
+        let audio = new Audio(`/sounds/${event.key}.mp3`);
+        audio.play();
+    }
     const p = document.createElement("p");
     p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
-    console.log(`The'${event.key}' key is pressed`);
-
-
-    /*if(`${event.key}`=="a") {
-        let audioObj = new Audio("/sounds/A.mp3");
-        audioObj.play();
-    }
-    if(`${event.key}`=="s") {
-        let audioObj = new Audio("/sounds/S.mp3");
-        audioObj.play();
-    }
-    if(`${event.key}`=="d") {
-        let audioObj = new Audio(`/sounds/${event.key}.mp3`);
-        audioObj.play();
-    }
-    if(`${event.key}`=="f") {
-        let audioObj = new Audio("/sounds/F.mp3");
-        audioObj.play();
-    }
-    if(`${event.key}`=="g") {
-        let audioObj = new Audio("/sounds/G.mp3");
-        audioObj.play();
-    }
-    if(`${event.key}`=="h") {
-        let audioObj = new Audio("/sounds/H.mp3");
-        audioObj.play();
-    }
-    if(`${event.key}`=="j") {
-        let audioObj = new Audio("/sounds/J.mp3");
-        audioObj.play();
-    }*/
-    /*document.getElementById("output").appendChild(p);*/
+    console.log(`The'${event.key}' key is pressed' | code='${event.code}'`);
 }, true);
-/*window.addEventListener("keydown", function(event) {
-    document.getElementById(`${event.key}`).style.backgroundColor = "red";
-
-}, true);
-window.addEventListener("keyup", function(event) {
-    document.getElementById(`${event.key}`).style.backgroundColor = "white";
-}, true);*/
